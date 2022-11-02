f8bf4df815a990a67b25251c2de57176b995a424
Edi Wang <Edi.Wang@outlook.com>
2022-11-02 12:08:48 +0800

    skip good mime type, use Spectre.Console

--- src/Program.cs
+++ src/Program.cs
@@ -1,20 +1,23 @@
 ﻿using System;
 using System.IO;
+using System.Reflection;
+using System.Text;
 using System.Threading.Tasks;
 using Azure.Storage.Blobs;
 using Azure.Storage.Blobs.Models;
 using CommandLine;
 using Microsoft.AspNetCore.StaticFiles;
+using Spectre.Console;
 
 namespace AzureBlobMimeTypeReset;
 
 class Options
 {
-    [Option('s', Required = true, HelpText = "Connection String")]
+    [Option(longName: "connection", HelpText = "Storage Account Connection String")]
     public string ConnectionString { get; set; }
 
-    [Option('c', Required = true, HelpText = "Blob Container Name")]
-    public string ContainerName { get; set; }
+    [Option(longName: "container", HelpText = "Blob Container Name")]
+    public string Container { get; set; }
 }
 
 class Program
@@ -23,55 +26,84 @@ class Program
 
     public static BlobContainerClient BlobContainer { get; set; }
 
-    static async Task Main(string[] args)
+    private static async Task Main(string[] args)
     {
+        Console.OutputEncoding = Encoding.UTF8;
+
         var parserResult = Parser.Default.ParseArguments<Options>(args);
         if (parserResult.Tag == ParserResultType.Parsed)
         {
             Options = ((Parsed<Options>)parserResult).Value;
 
-            // 1. Get Azure Blob Files
-            WriteMessage($"[{DateTime.Now}] Finding Files on Azure Blob Storage...");
-            BlobContainer = GetBlobContainer();
-            if (null == BlobContainer)
+            if (string.IsNullOrWhiteSpace(Options.ConnectionString))
             {
-                WriteMessage("ERROR: Can not get BlobContainer.", ConsoleColor.Red);
-                Console.ReadKey();
-                return;
+                Options.ConnectionString = AnsiConsole.Ask<string>("Enter Azure Storage Account connection string: ");
             }
 
-            // 2. Update Mime Type
-            var pvd = new FileExtensionContentTypeProvider();
-            WriteMessage($"[{DateTime.Now}] Updating Mime Type...");
-            int affectedFilesCount = 0;
-
-            await foreach (var blob in BlobContainer.GetBlobsAsync())
+            if (string.IsNullOrWhiteSpace(Options.Container))
             {
-                string extension = Path.GetExtension(blob.Name)?.ToLower();
-                bool isKnownType = pvd.TryGetContentType(extension, out string mimeType);
-                if (isKnownType)
+                Options.Container = AnsiConsole.Ask<string>("Enter container name: ");
+            }
+
+            WriteParameterTable();
+
+            if (!AnsiConsole.Confirm("Good to go?")) return;
+
+            try
+            {
+                BlobContainer = GetBlobContainer();
+
+                var pvd = new FileExtensionContentTypeProvider();
+                int affectedFilesCount = 0;
+
+                await foreach (var blob in BlobContainer.GetBlobsAsync())
                 {
-                    try
+                    AnsiConsole.MarkupLine($"Inspecting [green]'{blob.Name}'[/]");
+                    
+                    string extension = Path.GetExtension(blob.Name)?.ToLower();
+                    bool isKnownType = pvd.TryGetContentType(extension, out string mimeType);
+                    if (isKnownType && string.Compare(blob.Properties.ContentType, mimeType, StringComparison.OrdinalIgnoreCase) != 0)
                     {
-                        await SetContentType(blob.Name, mimeType);
-                        affectedFilesCount++;
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine(e);
+                        try
+                        {
+                            await SetContentType(blob.Name, mimeType);
+                            affectedFilesCount++;
+                        }
+                        catch (Exception e)
+                        {
+                            AnsiConsole.WriteException(e);
+                        }
                     }
                 }
-            }
 
-            WriteMessage($"[{DateTime.Now}] Update completed, {affectedFilesCount} file(s) updated.");
+                AnsiConsole.MarkupLine($"Update completed, [green]{affectedFilesCount}[/] file(s) updated.");
+            }
+            catch (Exception e)
+            {
+                AnsiConsole.WriteException(e);
+            }
         }
 
         Console.ReadKey();
     }
 
+    private static void WriteParameterTable()
+    {
+        var appVersion = Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
+        var table = new Table
+        {
+            Title = new($"AzureBlobMimeTypeReset {appVersion}")
+        };
+
+        table.AddColumn("Parameter");
+        table.AddColumn("Value");
+        table.AddRow(new Markup("[blue]Container Name[/]"), new Text(Options.Container));
+        AnsiConsole.Write(table);
+    }
+
     private static async Task SetContentType(string blobName, string contentType)
     {
-        WriteMessage($"[{DateTime.Now}] Updating {blobName} => {contentType}");
+        AnsiConsole.MarkupLine($"Updating [green]'{blobName}'[/] => [green]'{contentType}'[/]");
 
         var bc = BlobContainer.GetBlobClient(blobName);
         var headers = new BlobHttpHeaders
@@ -84,19 +116,9 @@ class Program
         await bc.SetHttpHeadersAsync(headers);
     }
 
-    private static void WriteMessage(string message, ConsoleColor color = ConsoleColor.White, bool resetColor = true)
-    {
-        Console.ForegroundColor = color;
-        Console.WriteLine(message);
-        if (resetColor)
-        {
-            Console.ResetColor();
-        }
-    }
-
     private static BlobContainerClient GetBlobContainer()
     {
-        BlobContainerClient container = new(Options.ConnectionString, Options.ContainerName);
+        BlobContainerClient container = new(Options.ConnectionString, Options.Container);
         return container;
     }
 }
